0e4d34175854f2d7e70f8ce57531dfbc98401a14
sher.lock <sher.lock@kakaoenterprise.com>
2020-03-06 14:01:00 +0900

    JS Clock (LocalStorage Used)

--- greeting.js
+++ greeting.js
@@ -1,8 +1,46 @@
 const form = document.querySelector('.js-form'),
-  input = form.querySelector('input');
+  input = form.querySelector('input'),
+  greeting = document.querySelector('.js-greetings');
 
+const USER_LS = 'currentUser',
+SHOWING_CN = 'showing';
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  // Submit 의 기본 동작은 글을 쓰고 엔터치면 어디론가 데이터를 보내버리고 document 는 새로고침 때려버림.
+  // 그걸 하기 싫다! 디폴트 막기.
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener('submit', handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerHTML = `Hello ${text}`;
+}
+
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
 
 function init() {
+  loadName();
 }
 
 init();
